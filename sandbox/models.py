9171c974d712fc48287796c53e792b1ee379f7e6
Igor Ferreira <igorkmail@gmail.com>
2025-07-02 21:01:30 -0300

    update sandbox

--- sandbox/models.py
+++ sandbox/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+
 # Models for the sandbox app (for experiments and learning)
+
+class Algorithm(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    code = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- urls.py
+++ urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseRedirect
 
-def redirect_to_api(request):
-    return HttpResponseRedirect('/api/')
+def redirect_to_sandbox(request):
+    return HttpResponseRedirect('/sandbox/')
 
 urlpatterns = [
-    path('', redirect_to_api, name='home'),
+    path('', redirect_to_sandbox, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('randori_doctor_api.urls')),
     path('sandbox/', include('sandbox.urls')),
