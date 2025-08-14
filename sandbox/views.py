b916ac29936e291a8ee4aa60ee1032c85dcdb092
Igor Ferreira <igorkmail@gmail.com>
2025-08-13 23:55:49 -0300

    Keep studyng

--- sandbox/views.py
+++ sandbox/views.py
@@ -26,6 +26,36 @@ ALGORITHMS = [
         'name': 'Binary Search',
         'description': 'Busca binária em array ordenado',
         'function': 'binary_search'
+    },
+    {
+        'id': 5,
+        'name': 'Valid Palindrome',
+        'description': 'Verificar se uma string é palíndromo',
+        'function': 'valid_palindrome'
+    },
+    {
+        'id': 6,
+        'name': 'Remove Duplicates',
+        'description': 'Remover duplicatas de array ordenado',
+        'function': 'remove_duplicates'
+    },
+    {
+        'id': 7,
+        'name': 'Merge Sorted Arrays',
+        'description': 'Combinar dois arrays ordenados',
+        'function': 'merge_sorted_arrays'
+    },
+    {
+        'id': 8,
+        'name': 'Contains Duplicate',
+        'description': 'Verificar se há duplicatas no array',
+        'function': 'contains_duplicate'
+    },
+    {
+        'id': 9,
+        'name': 'Valid Anagram',
+        'description': 'Verificar se duas strings são anagramas',
+        'function': 'valid_anagram'
     }
 ]
 
@@ -58,6 +88,16 @@ def execute_algorithm(function_name):
         return fibonacci(10)
     elif function_name == 'binary_search':
         return binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)
+    elif function_name == 'valid_palindrome':
+        return valid_palindrome("A man, a plan, a canal: Panama")
+    elif function_name == 'remove_duplicates':
+        return remove_duplicates([1, 1, 2, 2, 3, 4, 4, 5])
+    elif function_name == 'merge_sorted_arrays':
+        return merge_sorted_arrays([1, 3, 5], [2, 4, 6])
+    elif function_name == 'contains_duplicate':
+        return contains_duplicate([1, 2, 3, 1])
+    elif function_name == 'valid_anagram':
+        return valid_anagram("listen", "silent")
     else:
         return "Algoritmo não implementado"
 
@@ -96,3 +136,44 @@ def binary_search(arr, target):
             right = mid - 1
     
     return -1
+
+def valid_palindrome(s):
+    """Verificar se uma string é palíndromo"""
+    cleaned = ''.join(char.lower() for char in s if char.isalnum())
+    return cleaned == cleaned[::-1]
+
+def remove_duplicates(nums):
+    """Remover duplicatas de array ordenado"""
+    if not nums:
+        return []
+    
+    result = [nums[0]]
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            result.append(nums[i])
+    return result
+
+def merge_sorted_arrays(nums1, nums2):
+    """Combinar dois arrays ordenados"""
+    result = []
+    i = j = 0
+    
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] <= nums2[j]:
+            result.append(nums1[i])
+            i += 1
+        else:
+            result.append(nums2[j])
+            j += 1
+    
+    result.extend(nums1[i:])
+    result.extend(nums2[j:])
+    return result
+
+def contains_duplicate(nums):
+    """Verificar se há duplicatas no array"""
+    return len(nums) != len(set(nums))
+
+def valid_anagram(s, t):
+    """Verificar se duas strings são anagramas"""
+    return sorted(s.lower()) == sorted(t.lower())
